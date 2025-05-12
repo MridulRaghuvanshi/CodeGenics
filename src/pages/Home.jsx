ffa77322cc3e3a4ee4860a9ac71c4e39217ebfd0
Shrutishakya07 <shrutishakya2002@gmail.com>
2025-05-12 19:34:32 +0530

    Creating the basic structure which includes components and pages, creating and styling the navbar on the home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div className="w-full ">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
